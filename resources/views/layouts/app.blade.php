6c3f704ade175f2dac5a048e019b85deeecb1ed5
Josh <31908788+truemiller@users.noreply.github.com>
2022-06-14 20:46:54 +0100

    test

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,25 +1,23 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <link rel="prefetch" href="{{config('app.url')}}">
     <!-- Google Tag Manager -->
     <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
                 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
         })(window,document,'script','dataLayer','GTM-WH2KTPT');</script>
-
     <!-- End Google Tag Manager -->
 
-    {{--    GA and Tag Manager --}}
+{{--    GA and Tag Manager --}}
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-148739386-1"></script>
-    gtag('config', 'UA-148739386-1');
-    </script>
     <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
+
+        gtag('config', 'UA-148739386-1');
+    </script>
 
 
 
@@ -30,11 +28,12 @@
     <meta name="theme-color" content="#ffffff">
     {{--    Title--}}
     <title>@yield('title') Alterlister</title>
-    @yield('ld+json')
+@yield('ld+json')
 
-    @yield('og_tags')
-    <!-- Scripts -->
+@yield('og_tags')
+<!-- Scripts -->
     <link rel="canonical" href="{{url()->current()}}">
+    <link rel="prefetch" href="{{config('app.url')}}">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <style>
         button {
