07c93bb95b61dee491ea481347976fd1bcb06548
Josh <31908788+truemiller@users.noreply.github.com>
2022-06-17 23:46:46 +0100

    alternatives schema

--- resources/views/entity/alternatives.blade.php
+++ resources/views/entity/alternatives.blade.php
@@ -1,8 +1,9 @@
 <div class="card">
     <div class="card-body">
-        <h2 id="alternatives" >Alternatives to {{$entity->title}}</h2>
+        <h2 id="alternatives">Alternatives to {{$entity->title}}</h2>
         @foreach($alternatives as $alternative)
-            <section class="row py-3" id="{{$alternative->slug}}"  itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
+            <section class="row py-3" id="{{$alternative->slug}}" itemprop="itemListElement" itemscope
+                     itemtype="https://schema.org/ListItem">
                 <div class="col-lg-12 border-top pt-3">
                     <h2 class="fw-bolder">
                         <img src="{{$alternative->logo}}" alt="{{$alternative->title}} logo"
@@ -12,8 +13,8 @@
                             href="{{route('ent', ["ent"=>$alternative->slug])}}"
                             rel="external"
                             class="text-primary"
-                            title="{{$alternative->title}}"
-                            itemprop="name">{{$alternative->title}}</a>
+                            title="{{$alternative->title}} alternatives"
+                        ><span itemprop="name">{{$alternative->title}}</span></a>
                     </h2>
                     <ul>
                         <li><strong>Platforms</strong>: @foreach($alternative->platforms as $platform)<span
