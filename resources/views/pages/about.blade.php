b613037c01488e1d05684ad8ea31739e6a4f9f97
Josh <31908788+truemiller@users.noreply.github.com>
2022-06-19 10:06:56 +0100

    schema

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -2,9 +2,9 @@
 
 @section('main')
     <div class="container mt-3">
-        <div class="card">
-            <div class="card-body" itemscope itemtype="https://schema.org/Article">
-                <h1 itemprop="name">About Alterlister</h1>
+        <div class="card" itemscope itemtype="https://schema.org/AboutPage">
+            <div class="card-body" itemprop="mainContentOfPage">
+                <h1 itemprop="name" itemprop="headline">About <span itemprop="about">Alterlister</span></h1>
                 <p itemprop="text">Alterlister is focussed on listing alternatives to popular media, software, apps.</p>
             </div>
         </div>
