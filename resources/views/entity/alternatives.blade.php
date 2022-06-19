bb2b722612a299010ee7bbf85471ce8bbcaf095a
Josh <31908788+truemiller@users.noreply.github.com>
2022-06-19 19:22:24 +0100

    categories

--- resources/views/entity/alternatives.blade.php
+++ resources/views/entity/alternatives.blade.php
@@ -13,7 +13,7 @@
                          itemprop="logo"
                     >
                     <h2 class="fw-bolder d-inline-block">
-                        <span itemprop="position">{{$loop->index+1}}</span>. <a
+                        <span >{{$loop->index+1}}</span>. <a
                             href="{{route('ent', ["ent"=>$alternative->slug])}}"
                             rel="external"
                             class="text-primary"
@@ -23,12 +23,12 @@
                     <ul>
                         <li><strong>Category</strong>: <a href="/category/{{$alternative?->parent?->slug}}" itemprop="category">{{$alternative?->parent?->title}}</a></li>
                         <li><strong>Platforms</strong>: @foreach($alternative->platforms as $platform)<span
-                                class="badge bg-light me-1"  itemprop="operatingSystem"><i class="{{$platform->fa}}"></i> {{$platform->title}}</span>@endforeach</li>
+                                class="badge bg-light me-1" ><i class="{{$platform->fa}}"></i> {{$platform->title}}</span>@endforeach</li>
                         <li><strong>Tags</strong>: @foreach($alternative->tags as $tag)<span
                                 class="badge bg-light me-1" itemprop="keywords">{{$tag->tag}}</span>@endforeach</li>
                     </ul>
                     @if($alternative->image_1)
-                        <img src="{{$alternative->image_1}}" alt="An image of {{$alternative->title}}." title="An image of {{$alternative->title}}." width="804" class="mb-3 w-100" loading="lazy" itemprop="screenshot">
+                        <img src="{{$alternative->image_1}}" alt="An image of {{$alternative->title}}." title="An image of {{$alternative->title}}." width="804" class="mb-3 w-100" loading="lazy" itemprop="image">
                     @endif
                     <p itemprop="description">{!! nl2br(e($alternative->description)) !!}</p>
                     <a href="{{route('ent', ["ent"=>$alternative->slug])}}" class="btn btn-outline-primary">More
