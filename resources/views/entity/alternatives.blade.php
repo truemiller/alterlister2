0ede6811339587a1ee9f35247f3a2ad9de7653c4
Josh <31908788+truemiller@users.noreply.github.com>
2022-06-20 11:28:13 +0100

    aggregate rating

--- resources/views/entity/alternatives.blade.php
+++ resources/views/entity/alternatives.blade.php
@@ -13,23 +13,31 @@
                          itemprop="logo"
                     >
                     <h2 class="fw-bolder d-inline-block">
-                        <span >{{$loop->index+1}}</span>. <a
+                        <span>{{$loop->index+1}}</span>. <a
                             href="{{route('ent', ["ent"=>$alternative->slug])}}"
                             rel="external"
                             class="text-primary"
                             title="{{$alternative->title}} alternatives"
                         ><span itemprop="name">{{$alternative->title}}</span></a>
                     </h2>
-                    <meta content="{{$alternative->reviews->avg("stars") ?? 0 }}" itemprop="aggregateRating">
+                    <div class="rating" itemprop="aggregateRating" itemtype="https://schema.org/AggregateRating">
+                        <meta content="{{$alternative->reviews->avg("stars") ?? 0 }}" itemprop="ratingValue">
+                        <meta content="5" itemprop="bestRating">
+                        <meta content="{{$alternative->reviews->count()??0}}" itemprop="reviewCount">
+                    </div>
                     <ul>
-                        <li><strong>Category</strong>: <a href="/category/{{$alternative?->parent?->slug}}" itemprop="category">{{$alternative?->parent?->title}}</a></li>
+                        <li><strong>Category</strong>: <a href="/category/{{$alternative?->parent?->slug}}"
+                                                          itemprop="category">{{$alternative?->parent?->title}}</a></li>
                         <li><strong>Platforms</strong>: @foreach($alternative->platforms as $platform)<span
-                                class="badge bg-light me-1" ><i class="{{$platform->fa}}"></i> {{$platform->title}}</span>@endforeach</li>
+                                class="badge bg-light me-1"><i
+                                    class="{{$platform->fa}}"></i> {{$platform->title}}</span>@endforeach</li>
                         <li><strong>Tags</strong>: @foreach($alternative->tags as $tag)<span
                                 class="badge bg-light me-1" itemprop="keywords">{{$tag->tag}}</span>@endforeach</li>
                     </ul>
                     @if($alternative->image_1)
-                        <img src="{{$alternative->image_1}}" alt="An image of {{$alternative->title}}." title="An image of {{$alternative->title}}." width="804" class="mb-3 w-100" loading="lazy" itemprop="image">
+                        <img src="{{$alternative->image_1}}" alt="An image of {{$alternative->title}}."
+                             title="An image of {{$alternative->title}}." width="804" class="mb-3 w-100" loading="lazy"
+                             itemprop="image">
                     @endif
                     <p itemprop="description">{!! nl2br(e(Str::words($alternative->description, 50, "..."))) !!}</p>
                     <a href="{{route('ent', ["ent"=>$alternative->slug])}}" class="btn btn-outline-primary">More
