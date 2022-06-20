bb6537ca0ad83e98de3973be602717a264c2d0b3
Josh <31908788+truemiller@users.noreply.github.com>
2022-06-20 01:08:42 +0100

    excerpt on alternatives

--- resources/views/entity/alternatives.blade.php
+++ resources/views/entity/alternatives.blade.php
@@ -30,7 +30,7 @@
                     @if($alternative->image_1)
                         <img src="{{$alternative->image_1}}" alt="An image of {{$alternative->title}}." title="An image of {{$alternative->title}}." width="804" class="mb-3 w-100" loading="lazy" itemprop="image">
                     @endif
-                    <p itemprop="description">{!! nl2br(e($alternative->description)) !!}</p>
+                    <p itemprop="description">{!! nl2br(e(Str::words($alternative->description, 50, "..."))) !!}</p>
                     <a href="{{route('ent', ["ent"=>$alternative->slug])}}" class="btn btn-outline-primary">More
                         details</a>
                 </div>
