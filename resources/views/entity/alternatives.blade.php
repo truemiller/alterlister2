e218a32cb4b23eb30cc76bee424f136e465dfe57
Josh <31908788+truemiller@users.noreply.github.com>
2022-06-20 11:53:48 +0100

    aggregate rating

--- resources/views/entity/alternatives.blade.php
+++ resources/views/entity/alternatives.blade.php
@@ -28,7 +28,7 @@
                         <meta content="{{$alternative->reviews->avg("stars") ?? 0 }}" itemprop="ratingValue">
                         <meta content="5" itemprop="bestRating">
                         <meta content="0" itemprop="worstRating">
-                        <meta content="{{$alternative->reviews->count()??0}}" itemprop="reviewCount">
+                        <meta content="{{$alternative->reviews->count()??0}}" itemprop="ratingCount">
                     </div>
                     <ul>
                         <li><strong>Category</strong>: <a href="/category/{{$alternative?->parent?->slug}}"
