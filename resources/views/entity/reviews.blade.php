0984f405af9d381f268f04010f5f33a152896f8b
Josh <31908788+truemiller@users.noreply.github.com>
2022-06-21 17:29:34 +0100

    review schema default 5

--- resources/views/entity/reviews.blade.php
+++ resources/views/entity/reviews.blade.php
@@ -1,7 +1,7 @@
 <div class="card mt-3">
     <div class="card-body">
         <h2 id="reviews">Reviews</h2>
-        <div itemscope  itemtype="https://schema.org/AggregateRating">
+        <div itemscope itemprop="aggregateRating" itemtype="https://schema.org/AggregateRating">
             <meta content="{{$entity->reviews->count() === 0 ? 5 : $entity->reviews->count()}}" itemprop="ratingValue">
             <meta content="{{$entity->reviews->count() === 0 ? 1 : $entity->reviews->count() }}" itemprop="reviewCount">
         </div>
