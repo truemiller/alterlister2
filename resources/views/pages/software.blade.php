ed06a14014a68edb1e6447cb2ea6a99b0f8e0ddb
Josh <31908788+truemiller@users.noreply.github.com>
2022-06-22 12:59:50 +0100

    review schema default 5

--- resources/views/pages/software.blade.php
+++ resources/views/pages/software.blade.php
@@ -4,17 +4,19 @@
         <div class="card">
             <div class="card-body">
 
-                <table class="table table-striped table-sm">
+                <table class="table table-striped table-sm table-hover">
                     <thead>
                     <th>Title</th>
                     <th>Category</th>
                     <th>Link</th>
+                    <th>Reviews</th>
                     </thead>
                     @foreach(\App\Models\Entity::all()->sortBy("title") as $entity)
                         <tr>
                             <td>{{$entity->title}}</td>
                             <td>{{$entity->parent->title}}</td>
                             <td><a href="{{route('ent', ["ent"=>$entity->slug])}}#reviews">{{$entity->slug}}</a></td>
+                            <td>{{$entity->reviews->count()}}</td>
                         </tr>
                     @endforeach
                 </table>
